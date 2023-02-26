29e829dc3b34e0f8d889f8f4856394ba22797a90
Jens Keiner <jens.keiner@gmail.com>
2023-02-26 23:15:16 +0100

    Set package version.

--- exchange_calendars_extensions/__init__.py
+++ exchange_calendars_extensions/__init__.py
@@ -20,3 +20,24 @@ def set_extension(name, cls):
 
 
 __all__ = ["apply_extensions", "set_extension", "extend_class", "ExtendedExchangeCalendar"]
+
+__version__ = None
+
+try:
+    from importlib.metadata import version
+    # get version from installed package
+    __version__ = version("exchange_calendars_extensions")
+except ImportError:
+    pass
+
+if __version__ is None:
+    try:
+        # if package not installed, get version as set when package built
+        from ._version import version
+    except Exception:
+        # If package not installed and not built, leave __version__ as None
+        pass
+    else:
+        __version__ = version
+
+del version
